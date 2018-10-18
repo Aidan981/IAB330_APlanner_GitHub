4eca3aaa9899fe401439934b34b425942adfc394
Michael <Michael.K97@web.de>
2018-10-18 10:55:55 +1000

    First Layout

---
+++ APlanner/APlanner/Pages/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using APlanner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APlanner.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+    }
+}
