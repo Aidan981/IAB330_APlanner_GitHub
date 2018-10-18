f40f5218cc24fbf2807fa791d580737d5fcae970
Michael <Michael.K97@web.de>
2018-10-18 20:16:22 +1000

    Prototype - Master Detail Page

---
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using APlanner_MDP.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace APlanner_MDP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/Pages/MasterPageItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APlanner_MDP.Pages
+{
+    class MasterPageItem
+    {
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
