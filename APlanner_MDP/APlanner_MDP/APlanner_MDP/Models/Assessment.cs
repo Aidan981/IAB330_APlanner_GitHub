ab1a9f517834f427cc1f62d34e6ed011fb3d511e
Michael <Michael.K97@web.de>
2018-10-19 13:15:21 +1000

    Login Page added and Assesment Model added

---
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/Models/Assessment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APlanner_MDP.Models
+{
+    public class Assessment
+    {
+        public Assessment()
+        {
+            Name = "Assessment 1";
+            Unit = "IAB330";
+            Description = "Write a text.";
+            Link = "www.google.com";
+            Duedate = new DateTime(2018, 12, 2);
+        }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public DateTime Duedate { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using APlanner_MDP;
+using APlanner_MDP.Models;
+using APlanner_MDP.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APlanner_MDP.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            allAssessments = new Assessment();
+        }
+        private Assessment allAssessments;
+
+        public Assessment AllAssessments
+        {
+            get { return allAssessments; }
+            set
+            {
+                allAssessments = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
