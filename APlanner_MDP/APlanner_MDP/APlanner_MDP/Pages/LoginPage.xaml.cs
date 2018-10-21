06e3b0b25dbe85b2f1d90f7d407ab0e2398c244b
Sachintha Tharindu <ranganasachintha@gmail.com>
2018-10-22 03:12:59 +1000

    50% completed login

--- APlanner_MDP/APlanner_MDP/APlanner_MDP/Pages/LoginPage.xaml.cs
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/Pages/LoginPage.xaml.cs
@@ -12,9 +12,30 @@ namespace APlanner_MDP.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
-		public LoginPage ()
+
+        private string _username;
+        private string _password;
+        private bool _areCredentialsInvalid;
+
+
+        private bool UserAuthenticated(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username)
+                || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            return username.ToLowerInvariant() == "joe"
+                && password.ToLowerInvariant() == "secret";
+        }
+
+        public LoginPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+        }
+
+
+    }
 }
