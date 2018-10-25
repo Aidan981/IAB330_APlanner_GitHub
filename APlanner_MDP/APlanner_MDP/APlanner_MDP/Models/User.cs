050e3c56550ca1a53b824866839d538267390c69
Sachintha Tharindu <ranganasachintha@gmail.com>
2018-10-25 12:23:24 +1000

    test6

---
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APlanner_MDP.Models
+{
+    class user
+    {
+        private string text1;
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public user(string text)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public user(string text, string text1) : this(text)
+        {
+            this.text1 = text1;
+        }
+
+
+        public bool UserAuthenticated()
+        {
+
+
+
+            if (string.IsNullOrEmpty(this.Username)
+                || string.IsNullOrEmpty(this.Password))
+            {
+                return false;
+            }
+
+            return this.Username.ToLowerInvariant() == "joe"
+                && this.Password.ToLowerInvariant() == "secret";
+        }
+    }
+}
