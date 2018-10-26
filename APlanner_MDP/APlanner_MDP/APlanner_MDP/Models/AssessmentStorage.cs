6df174e6fcbf95025ab7c7716c162d4d082adb30
Michael <Michael.K97@web.de>
2018-10-26 10:45:31 +1000

    Last changes for the presentation

--- APlanner_MDP/APlanner_MDP/APlanner_MDP/Models/AssessmentStorage.cs
+++ APlanner_MDP/APlanner_MDP/APlanner_MDP/Models/AssessmentStorage.cs
@@ -12,10 +12,10 @@ namespace APlanner_MDP.Models
         public static ObservableCollection<Assessment> getAssessments(string username, string password)
         {
             return (new ObservableCollection<Assessment> {
-                new Assessment("Assessment 1","IAB330", new DateTime(2018,12,12),"You have to write a text.","www.google.com"),
-                new Assessment("Final Exam","CAB340", new DateTime(2018,12,12),"You have to write a text.","www.google.com"),
-                new Assessment("Assessment 3","CAB230", new DateTime(2018,08,10),"You have to write a text.","www.google.com"),
-                new Assessment("Assessment 4","EGB339", new DateTime(2018,12,12),"You have to write a text.","www.google.com")
+                new Assessment("Assessment 1","IAB330", new DateTime(2018,10,09,23,59,59),"You have to write a text.","www.google.com"),
+                new Assessment("Final Exam","CAB340", new DateTime(2018,10,18,10,30,0),"You have to write a text.","www.google.com"),
+                new Assessment("Assessment 3","CAB230", new DateTime(2018,11,01,10,0,0),"You have to write a text.","www.google.com"),
+                new Assessment("Assessment 4","EGB339", new DateTime(2018,11,01,23,59,59),"You have to write a text.","www.google.com")
             });
         }
     }
